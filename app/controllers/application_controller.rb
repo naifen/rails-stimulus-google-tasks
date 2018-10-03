fc3ec7b9e3d8a34136d237c506ff13198b07258a
JerryG <jgus2012@gmail.com>
2018-10-03 05:25:32 -0700

    initial commit webpack basic setup

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,5 @@
+require "turbolinks/redirection"
+
+class ApplicationController < ActionController::Base
+  include Turbolinks::Redirection # add this line b/c turbolinks require: false
+end
---
+++ app/frontend/packs/application.js
@@ -0,0 +1,25 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+console.log('Hello World from Webpacker')
+
+import * as ActiveStorage from "activestorage";
+import Rails from 'rails-ujs';
+import Turbolinks from 'turbolinks';
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+import "./application.css";
+
+ActiveStorage.start();
+Rails.start();
+Turbolinks.start();
+
+const application = Application.start();
+const context = require.context("controllers", true, /.js$/);
+application.load(definitionsFromContext(context));
