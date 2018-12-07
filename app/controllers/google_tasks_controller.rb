e6f7a33438b9dbbc8e790d9bf0add4e9f7440116
JerryG <jgus2012@gmail.com>
2018-12-07 04:11:52 -0800

    use stimulus to lisiten to Rails ujs ajax event and update dom

--- app/controllers/google_tasks_controller.rb
+++ app/controllers/google_tasks_controller.rb
@@ -8,28 +8,35 @@
 class GoogleTasksController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_not_connected, only: [:generate_url, :authorize]
-  before_action :ensure_connected, only: [:index, :create, :update, :destroy]
+  before_action :ensure_connected, except: [:generate_url, :authorize]
 
   def generate_url
     credentials = google_tasks_auth_params[:credentials].to_s
     gtasks_service = GoogleTasksService.new(credentials, current_user.username)
     url = gtasks_service.get_auth_url
-    js_res = url_js_res(is_authorized: url == GoogleTasksService::ALREADY_AUTHORIZED,
-                        url: url)
+    res = {
+      is_manipulate_dom: true,
+      selector: "#auth-url",
+      is_authorized: url == GoogleTasksService::ALREADY_AUTHORIZED,
+      content: url
+    }
     respond_to do |format|
-      format.js { render js: js_res }
+      format.json { render json: res.to_json }
     end
   end
 
   def authorize
     credentials = google_tasks_auth_params[:credentials].to_s
     code = google_tasks_auth_params[:auth_code].to_s
-    username = current_user.username
-    gtasks_service = GoogleTasksService.new(credentials, username)
+    gtasks_service = GoogleTasksService.new(credentials, current_user.username)
     stored_credentials = gtasks_service.get_and_store_credentials(code)
-    res_content = "Something went wrong, please try again."
-
-    # Store credentials json and auth code in redis for later Google Tasks auth
+    error_res = {
+      display_notification: true,
+      notification: {
+        content: "Something went wrong, please try again.",
+        type: "danger"
+      }
+    } # Stimulus homeController listen to this response and display notification
     respond_to do |format|
       if stored_credentials.present?
         begin
@@ -38,15 +45,15 @@ class GoogleTasksController < ApplicationController
           $redis.set("#{GoogleTasksService::AUTH_CODE_KEY_PREFIX}#{username}",
                     code)
         rescue StandardError => e
-          puts e.inspect #TODO clean up, using flash.now and partial?
-          format.js { render js: notification_for(res_content) }
+          puts e.inspect
+          format.json { render json: error_res.to_json }
         else
-          res_content = "Google Tasks successfully connected."
+          flash_content = "Google Tasks successfully connected."
           format.html { redirect_to google_tasks_path,
-                                    flash: { success: res_content } }
+                                    flash: { success: flash_content } }
         end
       else
-        format.js { render js: notification_for(res_content) }
+        format.json { render json: error_res.to_json }
       end
     end
   end
@@ -72,13 +79,6 @@ class GoogleTasksController < ApplicationController
       params.require(:google_tasks_auth).permit(:credentials, :auth_code)
     end
 
-    def url_js_res(is_authorized: false, url: "")
-      res = is_authorized ?
-            "target.textContent='#{url}';" :
-            "target.href='#{url}';target.textContent='Get the authorization code';"
-      "const target=document.querySelector('#auth-url');" + res
-    end
-
     def ensure_not_connected
       if helpers.is_google_tasks_connected_for(current_user.username)
         redirect_to google_tasks_path,
@@ -93,14 +93,5 @@ class GoogleTasksController < ApplicationController
                     notice: "Please connect your Google Tasks Service first."
         return false
       end
-    end
-
-    # TODO: move to helper?
-    def notification_for(content)
-      'const nav = document.querySelector("nav");' +
-        'const div = document.createElement("div");' +
-        'div.className = "notification-top-right notification is-danger";' +
-        "div.innerHTML = '<button class=\"delete\"></button>#{content}';" +
-        'document.body.insertBefore(div, nav);'
     end
 end
--- app/frontend/controllers/home_controller.ts
+++ app/frontend/controllers/home_controller.ts
@@ -7,6 +7,16 @@ class HomeController extends Controller {
   private hiddenCredentialsTarget: HTMLInputElement;
   private authCodeTarget: HTMLInputElement;
 
+  connect() {
+    document.body.addEventListener("ajax:success", this.xhrSuccessListener);
+    document.body.addEventListener("ajax:error", this.xhrErrorListener);
+  }
+
+  disconnect() {
+    document.body.removeEventListener("ajax:success", this.xhrSuccessListener);
+    document.body.removeEventListener("ajax:error", this.xhrErrorListener);
+  }
+
   urlFormSubmit(e: Event) {
     if (!this.credentialsTarget.value) {
       e.preventDefault();
@@ -32,6 +42,42 @@ class HomeController extends Controller {
     notificationDiv.className = `notification-top-right notification is-${type}`;
     notificationDiv.innerHTML = `<button class="delete"></button>${content}`;
     document.body.insertBefore(notificationDiv, nav);
+  }
+
+  // Rails-ujs event handlers, arrow function binds this to current context
+  private xhrSuccessListener = (event: CustomEvent) => {
+    const res = event.detail[0];
+    if (res.display_notification) {
+      this.displayNotificationFor(
+        res.notification.content,
+        res.notification.type
+      );
+    }
+    if (res.is_manipulate_dom) {
+      this.updateContentFor(res.selector, res.content, res.is_authorized);
+    }
+  };
+
+  private xhrErrorListener = () => {
+    this.displayNotificationFor(
+      "Something went wrong please try again",
+      "danger"
+    );
+  };
+
+  // TODO make this more generic
+  private updateContentFor(
+    selector: string,
+    content: string,
+    isAuthorized: boolean
+  ) {
+    const target: HTMLAnchorElement = document.querySelector(selector);
+    if (isAuthorized) {
+      target.textContent = content; // Already authorized
+    } else {
+      target.href = content;
+      target.textContent = "Get the authorization code";
+    }
   }
 }
 
