bce77f3cd5e8c4b52f886c83235b9178dd55c39c
JerryG <jgus2012@gmail.com>
2018-12-10 22:46:33 -0800

    read redis setting from credentials.yml.enc

--- app/controllers/google_tasks_controller.rb
+++ app/controllers/google_tasks_controller.rb
@@ -28,7 +28,8 @@ class GoogleTasksController < ApplicationController
   def authorize
     credentials = google_tasks_auth_params[:credentials].to_s
     code = google_tasks_auth_params[:auth_code].to_s
-    gtasks_service = GoogleTasksService.new(credentials, current_user.username)
+    username = current_user.username
+    gtasks_service = GoogleTasksService.new(credentials, username)
     stored_credentials = gtasks_service.get_and_store_credentials(code)
     error_res = {
       display_notification: true,
--- app/helpers/google_tasks_helper.rb
+++ app/helpers/google_tasks_helper.rb
@@ -3,16 +3,22 @@
 require 'googleauth/stores/redis_token_store'
 
 module GoogleTasksHelper
-  # Check if Google::Auth::UserRefreshCredentials is stored in redis
+  # Check if Google::Auth::UserRefreshCredentials and credential json and
+  # auth code is stored in redis
   # @param [String] username
   # @return [Boolean]
   def is_google_tasks_connected_for(username)
-    prfix = Google::Auth::Stores::RedisTokenStore::DEFAULT_KEY_PREFIX
-    google_user_token_string = $redis.get("#{prfix}#{username}")
+    prefix = Google::Auth::Stores::RedisTokenStore::DEFAULT_KEY_PREFIX
+    cred_prefix = GoogleTasksService::CREDENTIALS_KEY_PREFIX
+    code_prefix = GoogleTasksService::AUTH_CODE_KEY_PREFIX
+    google_user_token_string = $redis.get("#{prefix}#{username}")
+    google_user_cred_string = $redis.get("#{cred_prefix}#{username}")
+    google_user_code_string = $redis.get("#{code_prefix}#{username}")
 
     google_user_token_string.present? &&
       google_user_token_string.include?("refresh_token") &&
       google_user_token_string.include?("access_token") &&
-      google_user_token_string.include?("expiration_time_millis")
+      google_user_token_string.include?("expiration_time_millis") &&
+      google_user_cred_string.present? && google_user_code_string.present?
   end
 end
